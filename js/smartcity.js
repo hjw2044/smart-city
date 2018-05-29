32120d1f895dff478767e8a895a82d6559241b57
hjw2044 <hjw2044@163.com>
2018-05-30 00:20:32 +0800

    0.20

--- js/smartcity.js
+++ js/smartcity.js
@@ -1,3 +1,26 @@
-/**
- * Created by Administrator on 2018/5/29.
- */
+$(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        //loop: true,
+        effect:"coverflow",
+        slidesPerView : 1.5,//设置为1.5时轮播的宽度可以不用设
+        centeredSlides : true,
+        initialSlide:1,
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // 如果需要滚动条
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
+});
